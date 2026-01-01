41141552003acb279614325770f7fe567419a604
Yuzucchi-surface <m.yuzucchi.2022@gmail.com>
2026-01-01 11:58:57 +0900

    feat(wezterm): use WSL as default domain

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,1 +1,13 @@
-return {}
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.default_domain = 'WSL:Ubuntu'
+config.wsl_domains = {
+    {
+        name = 'WSL:Ubuntu',
+	distribution = 'Ubuntu',
+	default_cwd = "~",
+    },
+}
+
+return config
